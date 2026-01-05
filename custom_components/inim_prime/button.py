9c3ffaa1a440efcd769c8b0ab59adae41ea07e39
Pitscheider <pitscheider@tuta.com>
2026-01-05 21:18:59 +0100

    Clear area alarm memory button added

--- custom_components/inim_prime/button.py
+++ custom_components/inim_prime/button.py
@@ -1,18 +1,13 @@
 from custom_components.inim_prime import InimPrimeDataUpdateCoordinator, DOMAIN
-from custom_components.inim_prime.entities.area import AreaStateSensor
-from custom_components.inim_prime.entities.zone import ZoneStateSensor
+from custom_components.inim_prime.entities.area import AreaModeSelect, ClearAreaAlarmMemoryButton
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
     coordinator: InimPrimeDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
-    zones = coordinator.data.zones
     areas = coordinator.data.areas
     entities = []
 
-    for zone in zones.values():
-        entities.append(ZoneStateSensor(coordinator, zone))
-
     for area in areas.values():
-        entities.append(AreaStateSensor(coordinator, area))
+        entities.append(ClearAreaAlarmMemoryButton(coordinator, area))
 
     async_add_entities(entities, update_before_add=True)
--- custom_components/inim_prime/entities/area.py
+++ custom_components/inim_prime/entities/area.py
@@ -1,4 +1,5 @@
 from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.components.button import ButtonEntity
 from homeassistant.components.select import SelectEntity
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
 
@@ -7,7 +8,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from custom_components.inim_prime import InimPrimeDataUpdateCoordinator, DOMAIN
 from homeassistant.helpers.entity import DeviceInfo
 
-from inim_prime.models.area import SetAreaModeRequest, AreaMode
+from inim_prime.models.area import SetAreaModeRequest, AreaMode, ClearAreaAlarmMemoryRequest
 
 
 def create_area_device_info(
@@ -45,28 +46,6 @@ class AreaStateSensor(CoordinatorEntity[InimPrimeDataUpdateCoordinator], SensorE
             return area.state.name
         return None
 
-# class AreaModeSensor(CoordinatorEntity[InimPrimeDataUpdateCoordinator], SensorEntity):
-#     def __init__(self, coordinator: InimPrimeDataUpdateCoordinator, area: AreaStatus):
-#         super().__init__(coordinator)
-#
-#         self.area_id = area.id
-#         self._attr_name = "Mode"
-#         self._attr_unique_id = f"{DOMAIN}_area_{area.id}_mode"
-#         self._attr_device_class = SensorDeviceClass.ENUM
-#         self._attr_options = [state.name for state in AreaMode]
-#         self._attr_icon = "mdi:shield-lock"
-#
-#         self._attr_device_info = create_area_device_info(
-#             area_id=self.area_id,
-#             area_name=area.name,
-#         )
-#
-#     @property
-#     def native_value(self) -> str | None:
-#         area = self.coordinator.data.areas.get(self.area_id)
-#         if area:
-#             return area.mode.name
-#         return None
 
 class AreaModeSelect(
     CoordinatorEntity[InimPrimeDataUpdateCoordinator],
@@ -110,6 +89,33 @@ class AreaModeSelect(
 
         await self.coordinator.client.set_area_mode(request)
         await self.coordinator.async_request_refresh()
+
+
+class ClearAreaAlarmMemoryButton(
+    CoordinatorEntity[InimPrimeDataUpdateCoordinator],
+    ButtonEntity,
+):
+    def __init__(self, coordinator: InimPrimeDataUpdateCoordinator, area):
+        super().__init__(coordinator)
+
+        self.area_id = area.id
+        self._attr_name = "Clear Alarm Memory"
+        self._attr_unique_id = f"{DOMAIN}_area_{area.id}_clear_alarm_memory"
+        self._attr_icon = "mdi:alarm-off"
+
+        self._attr_device_info = create_area_device_info(
+            area_id=self.area_id,
+            area_name=area.name,
+        )
+
+    async def async_press(self) -> None:
+        request = ClearAreaAlarmMemoryRequest(
+            area_id=self.area_id,
+        )
+
+        await self.coordinator.client.clear_area_alarm_memory(request)
+        await self.coordinator.async_request_refresh()
+
 
 class AreaAlarmMemoryBinarySensor(CoordinatorEntity[InimPrimeDataUpdateCoordinator], BinarySensorEntity):
 

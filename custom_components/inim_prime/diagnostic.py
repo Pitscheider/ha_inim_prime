0cc7059259b54c0563fefca78ecb00226c7cc463
Pitscheider <pitscheider@tuta.com>
2026-01-12 20:26:52 +0100

    Diagnostic test 2

--- custom_components/inim_prime/diagnostic.py
+++ custom_components/inim_prime/diagnostic.py
@@ -5,7 +5,7 @@ from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.device_registry import DeviceEntry
 
 from .const import DOMAIN, CONF_SERIAL_NUMBER
 
@@ -56,11 +56,14 @@ async def async_get_config_entry_diagnostics(
     }
 
 
+
 async def async_get_device_diagnostics(
-        hass: HomeAssistant, config_entry: ConfigEntry, device: dr.DeviceEntry
+        hass: HomeAssistant, config_entry: ConfigEntry, device: DeviceEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a device."""
     coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+
+
 
     # Extract the device type and ID from the device identifiers
     device_info = {}
